0f8f231c3fb02af86d15530820d05fb45499166f
Jenessa40 <jmolson11@mail.mccneb.edu>
2026-06-16 18:03:45 -0500

    help

---
+++ login_process.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+// Hardcoded login for now
+$valid_username = "AquaticAdventures24";
+$valid_password = "Mamabear1923!!"; // Replace with something strong
+
+if ($_POST['username'] === $valid_username && $_POST['password'] === $valid_password) {
+    $_SESSION['logged_in'] = true;
+    header("Location: edit_hours.php");
+    exit();
+} else {
+    echo "Invalid login.";
+}
+?>
